7138a14319126e1abd3741f263c1bca9abe4b4a5
Todd Wolfson <todd@twolfson.com>
2014-03-19 22:35:13 -0700

    Moving to render function. Not sure where to get started here =(

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var mustache = require('mustache');
+var template = 'hello {{world}}';
 
-console.log(mustache.parse('hello {{world}}'));
+console.log(mustache.parse(template));
+console.log(mustache.render(template, {world: 'moon'}));
