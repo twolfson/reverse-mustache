d9af51e6143b5a3e806ce193efb1e8f74c650cd7
Todd Wolfson <todd@twolfson.com>
2014-03-19 22:56:42 -0700

    Making proof of concept for prolog

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ var template = 'hello {{world}}';
 
 console.log(mustache.parse(template));
 console.log(mustache.render(template, {world: 'moon'}));
+
+// Works well in prolog. I am sure there is a better way to do it.
+// ['h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'] = ['h', 'e', 'l', 'l', 'o', X1, X2, X3, X4, X5]
