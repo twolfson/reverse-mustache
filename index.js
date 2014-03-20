bf607d6b5503e5cf5baa02858571c879119f030d
Todd Wolfson <todd@twolfson.com>
2014-03-19 22:59:42 -0700

    Moving onto conditionals

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var mustache = require('mustache');
-var template = 'hello {{world}}';
+var template = 'hello {{#world}}moon{{/world}}';
 
 console.log(mustache.parse(template));
-console.log(mustache.render(template, {world: 'moon'}));
+console.log(mustache.render(template, {world: true}));
 
 // Works well in prolog. I am sure there is a better way to do it.
 // ['h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'] = ['h', 'e', 'l', 'l', 'o', X1, X2, X3, X4, X5]
