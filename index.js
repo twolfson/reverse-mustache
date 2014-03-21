da5723397ddd497e07dbd870d6e8faa647891436
Todd Wolfson <todd@twolfson.com>
2014-03-20 21:55:48 -0700

    Deciding to go another route for a bit

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ var template = 'hello {{#world}}moon{{/world}}';
 console.log(mustache.parse(template));
 console.log(mustache.render(template, {world: true}));
 
+// TODO: We should still probably draw out an automata diagram
+// But... let's re-invent regular expressions with booleans
+
 // Works well in prolog. I am sure there is a better way to do it.
 // ['h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'] = ['h', 'e', 'l', 'l', 'o', X1, X2, X3, X4, X5]
