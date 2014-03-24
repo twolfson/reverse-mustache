2fa200e1c493485410ab8e2a342878cf7c62afd2
Todd Wolfson <todd@twolfson.com>
2014-03-23 23:36:02 -0700

    Added test cases for inline conditionals

--- test/reverse-mustache_test.js
+++ test/reverse-mustache_test.js
@@ -17,7 +17,7 @@ var reverseMustacheUtils = {
 // TODO: Embrace the same flat file mantra as mustache.js
 // TODO: We should be able to reverse the mustache test suite
 
-describe.only('A mustache template without any mustache tokens', function () {
+describe('A mustache template without any mustache tokens', function () {
   describe('when reversed with matching content', function () {
     reverseMustacheUtils.save({
       template: 'hello moon',
@@ -70,6 +70,43 @@ describe('A mustache template with a terminating conditional token', function ()
   describe('when reversed with non-matching content', function () {
     reverseMustacheUtils.save({
       template: 'hello {{#world}}moon{{/world}}',
+      content: 'hello there'
+    });
+
+    it('returns `null`', function () {
+      expect(this.result).to.equal(null);
+    });
+  });
+});
+
+describe('A mustache template with an inline conditional token', function () {
+  describe('when reversed with matching content (boolean true)', function () {
+    reverseMustacheUtils.save({
+      template: 'hello {{#world}}there{{/world}} moon',
+      content: 'hello there moon'
+    });
+
+    it('returns meta information', function () {
+      expect(this.result).to.not.equal(null);
+      expect(this.result.tokensByName).to.deep.equal({world: true});
+    });
+  });
+
+  describe.only('when reversed with matching content (boolean false)', function () {
+    reverseMustacheUtils.save({
+      template: 'hello {{#world}}there{{/world}} moon',
+      content: 'hello moon'
+    });
+
+    it('returns meta information', function () {
+      expect(this.result).to.not.equal(null);
+      expect(this.result.tokensByName).to.deep.equal({world: false});
+    });
+  });
+
+  describe('when reversed with non-matching content', function () {
+    reverseMustacheUtils.save({
+      template: 'hello {{#world}}there{{/world}} moon',
       content: 'hello there'
     });
 
