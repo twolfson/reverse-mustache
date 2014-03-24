2a95bdf833ee68b83b0f6aafd77e61462e482229
Todd Wolfson <todd@twolfson.com>
2014-03-23 22:27:08 -0700

    Outlining test suite

--- test/reverse-mustache_test.js
+++ test/reverse-mustache_test.js
@@ -1,11 +1,37 @@
 var reverse_mustache = require('../');
 
-describe('reverse-mustache', function () {
-  before(function () {
+// TODO: We should be able to reverse the mustache test suite
 
+describe('A mustache template without any mustache tokens', function () {
+  describe('when reversed with matching content', function () {
+    it('returns meta information', function () {
+
+    });
   });
 
-  it('', function () {
+  describe.skip('when reversed with non-matching content', function () {
+    it('returns `null`', function () {
 
+    });
+  });
+});
+
+describe.skip('A mustache template with a conditional token', function () {
+  describe('when reversed with matching content (boolean true)', function () {
+    it('returns meta information', function () {
+
+    });
+  });
+
+  describe.skip('when reversed with matching content (boolean false)', function () {
+    it('returns meta information', function () {
+
+    });
+  });
+
+  describe.skip('when reversed with non-matching content', function () {
+    it('returns `null`', function () {
+
+    });
   });
 });
