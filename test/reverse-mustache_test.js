fb9e86ba2bda51fe68968d63f5980cbcd69051b5
Todd Wolfson <todd@twolfson.com>
2014-03-23 22:45:50 -0700

    Fixed up library and test suite to a "working" state

--- test/reverse-mustache_test.js
+++ test/reverse-mustache_test.js
@@ -1,10 +1,22 @@
 // Load in library
 var reverseMustache = require('../');
 
+// Define helper utilities
+var reverseMustacheUtils = {
+  save: function (params) {
+    before(function callReverseMustache () {
+      this.result = reverseMustache(params);
+    });
+    after(function cleanupResult () {
+      delete this.result;
+    });
+  }
+};
+
 // TODO: Embrace the same flat file mantra as mustache.js
 // TODO: We should be able to reverse the mustache test suite
 
-describe('A mustache template without any mustache tokens', function () {
+describe.skip('A mustache template without any mustache tokens', function () {
   describe('when reversed with matching content', function () {
     it('returns meta information', function () {
 
@@ -18,7 +30,7 @@ describe('A mustache template without any mustache tokens', function () {
   });
 });
 
-describe.skip('A mustache template with a conditional token', function () {
+describe('A mustache template with a conditional token', function () {
   describe('when reversed with matching content (boolean true)', function () {
     reverseMustacheUtils.save({
       template: 'hello {{#world}}moon{{/world}}',
