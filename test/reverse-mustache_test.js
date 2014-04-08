44d3ba1da7cb848b3ab7c8fea1f5e9d7802a18a4
Todd Wolfson <todd@twolfson.com>
2014-04-08 00:19:55 -0700

    Removed dev .only and console.logs. All tests are looking green =3

--- test/reverse-mustache_test.js
+++ test/reverse-mustache_test.js
@@ -289,7 +289,7 @@ describe('A mustache template with variables in its loop', function () {
     });
   });
 
-  describe.only('using the dot notation context', function () {
+  describe('using the dot notation context', function () {
     describe('when reversed', function () {
       reverseMustacheUtils.save({
         template: '{{#place}}{{.}}{{/place}}',
