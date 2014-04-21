06fa17607e2d663ab5823c4ddfd7b62b7b8471a4
Todd Wolfson <todd@twolfson.com>
2014-04-20 23:27:18 -0700

    Added test for empty

---
+++ test/basic-empty_test.js
@@ -0,0 +1,39 @@
+var expect = require('chai').expect;
+var reverseMustacheUtils = require('./utils/reverse-mustache');
+
+describe.only('A mustache template with an empty token', function () {
+  describe('when reversed with matching content (boolean false)', function () {
+    reverseMustacheUtils.save({
+      template: 'hello{{^world}} moon{{/world}}',
+      content: 'hello'
+    });
+
+    it('returns meta information', function () {
+      expect(this.result).to.not.equal(null);
+      expect(this.result).to.deep.equal({world: false});
+    });
+  });
+
+  describe('when reversed with matching content (boolean true)', function () {
+    reverseMustacheUtils.save({
+      template: 'hello{{^world}} moon{{/world}}',
+      content: 'hello moon'
+    });
+
+    it('returns meta information', function () {
+      expect(this.result).to.not.equal(null);
+      expect(this.result).to.deep.equal({world: true});
+    });
+  });
+
+  describe('when reversed with non-matching content', function () {
+    reverseMustacheUtils.save({
+      template: 'hello {{^world}}moon{{/world}}',
+      content: 'hello there'
+    });
+
+    it('returns `null`', function () {
+      expect(this.result).to.equal(null);
+    });
+  });
+});
