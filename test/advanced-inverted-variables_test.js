b2cae29839bbe8928231d5cfb2dfc4fac94bf127
Todd Wolfson <todd@twolfson.com>
2014-04-24 00:03:17 -0700

    Added a test for nested inverted variables

--- test/advanced-inverted-variables_test.js
+++ test/advanced-inverted-variables_test.js
@@ -1,6 +1,20 @@
 var expect = require('chai').expect;
 var reverseMustacheUtils = require('./utils/reverse-mustache');
 
+describe('A mustache template with an nested inverted token', function () {
+  describe('when reversed', function () {
+    reverseMustacheUtils.save({
+      template: 'hello {{^world.place}}moon{{/world.place}}',
+      content: 'hello moon'
+    });
+
+    it('returns the nested object variable', function () {
+      expect(this.result).to.not.equal(null);
+      expect(this.result).to.deep.equal({world: {place: false}});
+    });
+  });
+});
+
 describe('A mustache template with an inverted token', function () {
   describe('when reversed with an inner variable', function () {
     reverseMustacheUtils.save({
