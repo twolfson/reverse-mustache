5222af599f0f0c03299cda3f58d6a9146fd45c7b
Todd Wolfson <todd@twolfson.com>
2014-04-24 02:44:11 -0700

    Removed dev TODOs

--- test/basic-text_test.js
+++ test/basic-text_test.js
@@ -2,9 +2,6 @@
 var expect = require('chai').expect;
 var reverseMustacheUtils = require('./utils/reverse-mustache');
 
-// TODO: Embrace the same flat file mantra as mustache.js
-// TODO: We should be able to reverse the mustache test suite
-
 describe('A mustache template without any mustache tokens', function () {
   describe('when reversed with matching content', function () {
     reverseMustacheUtils.save({
