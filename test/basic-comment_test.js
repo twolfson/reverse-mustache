6a6681e402145602a3b8a45b55f40c23ff9bf78e
Todd Wolfson <todd@twolfson.com>
2014-04-24 00:36:50 -0700

    Fixed up tests and added a noop reverse

--- test/basic-comment_test.js
+++ test/basic-comment_test.js
@@ -4,24 +4,13 @@ var reverseMustacheUtils = require('./utils/reverse-mustache');
 describe.only('A mustache template with a comment tokens', function () {
   describe('when reversed with matching content', function () {
     reverseMustacheUtils.save({
-      template: 'hello {{!moon}}',
-      content: 'hello <!-- moon -->'
+      template: 'hello{{!moon}}',
+      content: 'hello'
     });
 
-    it('returns meta information', function () {
+    it('is ignored and we receive meta information', function () {
       expect(this.result).to.not.equal(null);
       expect(this.result).to.be.an('object');
-    });
-  });
-
-  describe('when reversed with non-matching content', function () {
-    reverseMustacheUtils.save({
-      template: 'hello {{!moon}}',
-      content: 'hello moon'
-    });
-
-    it('returns `null`', function () {
-      expect(this.result).to.equal(null);
     });
   });
 });
