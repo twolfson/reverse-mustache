7b1213b0f01697509341c5c880c743f513becb0b
Todd Wolfson <todd@twolfson.com>
2014-04-24 02:45:09 -0700

    Removed TODOs which are more comments on reason for not supporting cases

--- test/advanced-loop-variables_test.js
+++ test/advanced-loop-variables_test.js
@@ -3,10 +3,10 @@ var expect = require('chai').expect;
 var reverseMustacheUtils = require('./utils/reverse-mustache');
 
 describe('A mustache template with variables in its loop', function () {
-  // TODO: There is no inheritance from inner loop to outer loop, we need to add that
-  // TODO: When we introduce this inheritance, we need to *not* set `true` for the original token becuase it will be applying under the namespace of the current item (or will it?) =_= -- {{#place}}{{name}}{{/place}} (was `name` global or `place.name`)?
+  // There is no inheritance from inner loop to outer loop, we need to add that
+  // When we introduce this inheritance, we need to *not* set `true` for the original token becuase it will be applying under the namespace of the current item (or will it?) =_= -- {{#place}}{{name}}{{/place}} (was `name` global or `place.name`)?
   // DEV: We are skipping this since it conflicts with `{{#place}}{{name}}{{/place}}` as it is impossible to tell whether `name` was a property of `place` or if `name` is a global
-  // TODO: This can be resolved via a `schema` or variable type hinting but that is not as elegant
+  // This could be resolved via a `schema` or variable type hinting but that is not as elegant
   /*
   describe('using the outer context', function () {
     describe('when reversed', function () {
