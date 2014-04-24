2a27926173ebedde103c9b9255e79285c73a9618
Todd Wolfson <todd@twolfson.com>
2014-04-24 00:55:39 -0700

    Added tests for partials

---
+++ test/basic-partial_test.js
@@ -0,0 +1,34 @@
+var expect = require('chai').expect;
+var reverseMustacheUtils = require('./utils/reverse-mustache');
+
+describe('A mustache template with a partial token', function () {
+  describe('when reversed', function () {
+    reverseMustacheUtils.save({
+      template: 'hello {{> place}}',
+      content: 'hello moon',
+      partials: {
+        place: 'moon'
+      }
+    });
+
+    it('returns meta information', function () {
+      expect(this.result).to.not.equal(null);
+      expect(this.result).to.deep.equal({place: 'moon'});
+    });
+  });
+
+  describe('when reversed in a loop', function () {
+    reverseMustacheUtils.save({
+      template: 'hello {{#place}}{{> name}}{{/place}}',
+      content: 'hello moon',
+      partials: {
+        place: '{{name}}'
+      }
+    });
+
+    it('returns meta information', function () {
+      expect(this.result).to.not.equal(null);
+      expect(this.result).to.deep.equal({place: {name: 'moon'}});
+    });
+  });
+});
