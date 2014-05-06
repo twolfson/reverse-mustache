b6162008ab5a581502f642becec380ba5f882b1c
Todd Wolfson <todd@twolfson.com>
2014-05-06 00:56:44 -0700

    Attempted to match regexp but running into size limitation o_o

--- test/performance_test.js
+++ test/performance_test.js
@@ -7,14 +7,17 @@ describe('A mustache template with a variable', function () {
     // 50,000 a's (x2) is equivalent to a beautified http://nitevibe.com/
     // DEV: See https://gist.github.com/twolfson/11136618
     var lotsOfAs = new Array(5e4).join('a');
-    reverseMustacheUtils.save({
-      template: lotsOfAs + 'aa {{place}}' + lotsOfAs,
-      content: lotsOfAs + 'aa moon' + lotsOfAs
-    });
+    // reverseMustacheUtils.save({
+    //   template: lotsOfAs + 'aa {{place}}' + lotsOfAs,
+    //   content: lotsOfAs + 'aa moon' + lotsOfAs
+    // });
+    var regexp = new RegExp(lotsOfAs + 'aa (.*)' + lotsOfAs);
+    var match = (lotsOfAs + 'aa moon' + lotsOfAs).match(regexp);
+    console.log(match[1]);
 
     it('returns meta information', function () {
-      expect(this.result).to.not.equal(null);
-      expect(this.result).to.deep.equal({place: 'moon'});
+      // expect(this.result).to.not.equal(null);
+      // expect(this.result).to.deep.equal({place: 'moon'});
     });
   });
 });
