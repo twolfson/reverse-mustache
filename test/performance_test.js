37eed17ab2cc92de7b68bf7a934defdb8a34d8f5
Todd Wolfson <todd@twolfson.com>
2014-05-06 00:57:06 -0700

    Regexp blows us out of the water at 3e4

--- test/performance_test.js
+++ test/performance_test.js
@@ -6,7 +6,7 @@ describe('A mustache template with a variable', function () {
   describe('when reversing a very large content block', function () {
     // 50,000 a's (x2) is equivalent to a beautified http://nitevibe.com/
     // DEV: See https://gist.github.com/twolfson/11136618
-    var lotsOfAs = new Array(5e4).join('a');
+    var lotsOfAs = new Array(3e4).join('a');
     // reverseMustacheUtils.save({
     //   template: lotsOfAs + 'aa {{place}}' + lotsOfAs,
     //   content: lotsOfAs + 'aa moon' + lotsOfAs
